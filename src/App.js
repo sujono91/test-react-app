91641ccef879ce3520d814b09e852d31e7263a27
sujono91 <sujono@etobee.com>
2017-07-22 00:13:16 +0700

    fix linting

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import './App.css'
 
 class App extends Component {
   cek() {
-    console.log('test')
+    console.log('test');
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"  >
+          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
