f578fc951b0f16aabd3121f30e2e14abf6a5d23e
sujono91 <sujono@etobee.com>
2017-07-21 23:58:41 +0700

    test Linting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
   cek() {
@@ -11,7 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo"  >
+          <img src={logo} className="App-logo" alt="logo" >
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
