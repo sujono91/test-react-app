5168dea908844bb333cebaa145e054532e395dc8
sujono91 <sujono@etobee.com>
2017-07-22 00:15:23 +0700

    fix linting

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import logo from './logo.svg'
 import './App.css'
 
 class App extends Component {
-  cek() {
-    console.log('test');
-  }
-
   render() {
     return (
       <div className="App">
